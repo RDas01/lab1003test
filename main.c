c57fbb70a2ff83bc5c35badbf3b694ddf5f388a8
Lachlan Mill <c3350378@uon.edu.au>
2020-02-27 08:51:56 +1100

    lab1test

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()  {
+    float x;
+    float y;
+    printf("Enter an number: ");
+    scanf("%f" ,&x); // scans for number
+    y = (x+2)/(x-1);
+    printf("y: %f\n", y);
+    return 0;
+}
